a67b66d87430427823223069707a5d0db18c59a8
ryanzinho20 <168549785+ryanzinho20@users.noreply.github.com>
2024-10-11 19:18:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é javascript?',
+    'O JavaScript é uma linguagem de programação.'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Franca?',
+    'A capital da Franca é Paris.'
+)
+criaCartao(
+    'Programação',
+        'O que é uma função?',
+        'É um bloco de código que executa alguma tarefa.'
+)
+criaCartao(
+    'Ingles',
+    'Como se diz oi em ingles?',
+    'Oi em ingles se diz Hi (rai).'
+)
